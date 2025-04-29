c2a889d4386397ddc1ee0d0e1d9306d3a6f26f98
ryohiy <ryohiyowork@gmail.com>
2025-04-29 22:31:58 +0900

    ui実装

--- ui/wasabi/src/lib.rs
+++ ui/wasabi/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#[no_std]
+extern crate alloc;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod app;
+mod cursor;
