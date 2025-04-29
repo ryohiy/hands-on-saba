bbef3a172319eb172c654f4f19565d9a06ddf6b9
ryohiy <ryohiyowork@gmail.com>
2025-04-30 00:22:56 +0900

    GUIの実装

--- saba_core/src/renderer/page.rs
+++ saba_core/src/renderer/page.rs
@@ -5,6 +5,8 @@ use crate::renderer::css::cssom::CssParser;
 use crate::renderer::css::cssom::StyleSheet;
 use crate::renderer::css::token::CssTokenizer;
 use crate::renderer::dom::api::get_style_content;
+use crate::renderer::dom::node::ElementKind;
+use crate::renderer::dom::node::NodeKind;
 use crate::renderer::dom::node::Window;
 use crate::renderer::html::parser::HtmlParser;
 use crate::renderer::html::token::HtmlTokenizer;
@@ -37,13 +39,31 @@ impl Page {
         }
     }
 
+    pub fn clicked(&self, position: (i64, i64)) -> Option<String> {
+        let view = match &self.layout_view {
+            Some(v) => v,
+            None => return None,
+        };
+
+        if let Some(n) = view.find_node_by_position(position) {
+            if let Some(parent) = n.borrow().parent().upgrade() {
+                if let NodeKind::Element(e) = parent.borrow().node_kind() {
+                    if e.kind() == ElementKind::A {
+                        return e.get_attribute("href");
+                    }
+                }
+            }
+        }
+
+        None
+    }
+
     pub fn set_browser(&mut self, browser: Weak<RefCell<Browser>>) {
         self.browser = browser;
     }
 
     pub fn receive_response(&mut self, response: HttpResponse) {
         self.create_frame(response.body());
-        
 
         self.set_layout_view();
 
@@ -53,9 +73,6 @@ impl Page {
     fn create_frame(&mut self, html: String) {
         let html_tokenizer = HtmlTokenizer::new(html);
         let frame = HtmlParser::new(html_tokenizer).construct_tree();
-        // let mut debug_output = String::new();
-        // write!(&mut debug_output, "{:#?}", frame).unwrap();
-        // panic!("{}", debug_output);
         let dom = frame.borrow().document();
 
         let style = get_style_content(dom);
